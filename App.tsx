1d8dd3843a9211525d3963777c329bb77378469d
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-05-20 00:37:01 +0300

    Feat: setup navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import MoviesListing from './src/screens/MoviesListing/MoviesListing';
+import MovieDetails from './src/screens/MovieDetails/MovieDetails';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Movies" 
+          screenOptions={{
+            headerTintColor: 'white',
+            headerStyle: { backgroundColor: '#1976D2' },
+          }}
+          >
+          <Stack.Screen name="Movies" component={MoviesListing} />
+          <Stack.Screen name="Movie Details" component={MovieDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
