d99d4711cd59a74791cb1a963ba3543194f05e97
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-05-20 01:44:29 +0300

    Feat: fetch and list movies

--- src/screens/MoviesListing/MoviesListing.tsx
+++ src/screens/MoviesListing/MoviesListing.tsx
@@ -1,17 +1,66 @@
-import { FC } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
+import { Button, FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import  Movie  from "../../models/Movie";
+
+const apiKey = '38e4a7fa63dea0b2c5c9dc5ce87d503d';
 
 const MoviesListing: FC = ({ navigation }) => {
 
-  function handlePressedMovie() {
-    navigation.navigate('Movie Details');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handlePressedMovie(movieDetails: Movie) {
+    navigation.navigate('Movie Details', { movieDetails });
   }
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+        if(response) {
+          setMovies(response.data.results);
+          console.log(response.data.results);
+        }
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error fetching movies');
+      }
+      setIsLoading(false);
+    }
+
+    fetchMovies();
+  }, []);
 
 
   return (
     <View style={styles.container}>
       <Text>Movies Listing</Text>
-      <Button title="Go to Movie Details" onPress={handlePressedMovie} />
+      { isLoading && <Text>Loading...</Text> }
+
+      { !isLoading && 
+      
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => handlePressedMovie(item)}>
+            <View>
+              <Text>{item?.title}</Text>
+            </View>
+            <View>
+              <Image source={{ uri: `https://image.tmdb.org/t/p/w500${item?.poster_path}` }} style={{ width: 100, height: 100 }} />
+            </View>
+            <View>
+              <Text>{item?.release_date}</Text>
+            </View>
+            <View>
+              <Text>{item?.vote_average}</Text>
+            </View>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item?.id.toString()}></FlatList>}
     </View>
   );
 }
