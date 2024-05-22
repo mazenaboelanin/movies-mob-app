4486c649ab57bda42816335197653daa6ecbc488
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-05-23 00:15:35 +0300

    Feat: check internet connection and restart if use click reload

--- App.tsx
+++ App.tsx
@@ -2,13 +2,33 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, NativeModules, Alert } from 'react-native';
+import RNRestart from 'react-native-restart';
 import MoviesListing from './src/screens/MoviesListing/MoviesListing';
 import MovieDetails from './src/screens/MovieDetails/MovieDetails';
+import { addEventListener } from "@react-native-community/netinfo";
+import { useEffect} from "react";
+const { ToastAndroid } = NativeModules;
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    unsubscribe();
+  },[]);
+
+  const unsubscribe = addEventListener(state => {
+    if (state.isConnected) {
+      ToastAndroid.show("internet connection exists", ToastAndroid.SHORT);
+    } else if (!state.isConnected) {
+      console.log("**** DISCONNECTED ****");
+      Alert.alert("No internet connection", "Please reconnect to the internet to use the app", [
+        { text: "Reload App", onPress: () => RNRestart.restart() }
+      ]);
+    }
+  });
+
   return (
     <>
       <StatusBar style="light" />
